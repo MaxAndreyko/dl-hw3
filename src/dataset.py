619032992c030fde47d1e9f6de6445a1e43fdd27
MaxAndreyko <max_andreyko@mail.ru>
2024-06-11 21:50:42 +0300

    add pad_id

--- src/dataset.py
+++ src/dataset.py
@@ -12,7 +12,7 @@ class TextDataset(Dataset):
 
     def __init__(self, data_file: str, train: bool = True, sp_model_prefix: str = None,
                  vocab_size: int = 2000, normalization_rule_name: str = 'nmt_nfkc_cf',
-                 model_type: str = 'bpe', max_length: int = 128):
+                 model_type: str = 'bpe', max_length: int = 256, pad_id: int = 3):
         """
         Dataset with texts, supporting BPE tokenizer
         :param data_file: txt file containing texts
@@ -28,7 +28,7 @@ class TextDataset(Dataset):
             SentencePieceTrainer.train(
                 input=data_file, vocab_size=vocab_size,
                 model_type=model_type, model_prefix=sp_model_prefix,
-                normalization_rule_name=normalization_rule_name
+                normalization_rule_name=normalization_rule_name, pad_id=pad_id
             )
         # load tokenizer from file
         self.sp_model = SentencePieceProcessor(model_file=sp_model_prefix + '.model')
@@ -39,8 +39,8 @@ class TextDataset(Dataset):
         # Включаем разделение на обучающую и валидационную выборки в методе __init__
         self.train_texts, self.val_texts = self.split_train_val(texts, self.VAL_RATIO, self.TRAIN_VAL_RANDOM_SEED)
         self.texts = self.train_texts if train else self.val_texts
-        self.indices = [self.sp_model.encode(text) for text in self.texts] # FIX: Поменял texts на text
-
+        self.indices = [self.text2ids(text) for text in self.texts] # FIX: Поменял texts на text
+        
         self.pad_id, self.unk_id, self.bos_id, self.eos_id = \
             self.sp_model.pad_id(), self.sp_model.unk_id(), \
                 self.sp_model.bos_id(), self.sp_model.eos_id()
